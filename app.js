bcb31c333cfe05d462cdadf7afed4cff5b8a3712
Vitor Rafael <vitorafael.r.c@gmail.com>
2020-07-17 17:05:49 -0300

    mongo

--- app.js
+++ app.js
@@ -5,17 +5,26 @@
     const bodyParser = require('body-parser');
     const admin = require('./routes/admin');
     const path = require('path')
-    // const mongoose = require('mongoose');
+    const mongoose = require('mongoose');
 
 // Configurações
     // BodyParser
         app.use(bodyParser.urlencoded({extended:true}));
         app.use(bodyParser.json());
+
     // Handlebars 
         app.engine('handlebars', handlebars({defaultLayout: 'main'}));
         app.set('view engine', 'handlebars');
+
     // Mongoose
-        // Soon
+        mongoose.Promise = global.Promise;
+        mongoose.connect("mongodb://localhost/blogapp", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+          })
+          .then(() => console.log("Conectado ao mongo"))
+          .catch(err => console.log("erro ao se conectar no banco" + err))
+          
     // Public 
         app.use(express.static(path.join(__dirname, "public")));
 
