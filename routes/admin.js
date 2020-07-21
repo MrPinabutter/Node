bc1ec98612597dfd47fb0358dcc8d7a21b8c70bb
Vitor Rafael <vitorafael.r.c@gmail.com>
2020-07-20 23:00:55 -0300

    postagens

--- routes/admin.js
+++ routes/admin.js
@@ -128,4 +128,17 @@ router.post("/categorias/deletar", (req, res) => {
     })
 })
 
+router.get("/postagens", (req, res) => {
+    res.render("admin/postagens")
+})
+
+router.get("/postagens/add", (req, res) => {
+    Categoria.find().lean().then(categorias => {
+        res.render("admin/addpostagem", {categorias: categorias})
+    }).catch(err => { 
+        req.flash("erros_msg", "houve um erro ao carregar o formulário")
+        res.redirect("/admin")
+    })
+})
+
 module.exports = router
